5d2853d5a11dfdff79b7a550fbed546b24d7730a
om praksash pattjoshi <omprakashpattjoshi123@gmail.com>
2022-03-27 11:35:29 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB761UZH4dX5LalMhfinl9EpvW_VMAwOdY",
+    authDomain: "crypto-hunter-92155.firebaseapp.com",
+    projectId: "crypto-hunter-92155",
+    storageBucket: "crypto-hunter-92155.appspot.com",
+    messagingSenderId: "286942384437",
+    appId: "1:286942384437:web:a6586b4dfb04cc9b547c7c"
+  };
+
+export default firebaseConfig;
+
+
